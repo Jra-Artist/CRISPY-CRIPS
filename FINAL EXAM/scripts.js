2b38cf503e5527ca5d9c037239f727708ccfa23c
John Robert Arevalo <146354712+Jra-Artist@users.noreply.github.com>
2024-01-25 18:10:17 +0800

    Add files via upload

---
+++ FINAL EXAM/scripts.js
@@ -0,0 +1,14 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Smooth scroll to the target element when a navigation link is clicked
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    });
+
